1814c69d6773dd6b82c64b66b93a0bfc0966c51b
GGedde <ggedde@users.noreply.github.com>
2019-05-02 16:18:18 -0700

    Included spinner

--- opsworks_deploy.sh
+++ opsworks_deploy.sh
@@ -4,7 +4,60 @@
 
 DIR="$(dirname "$0")"
 
-source ${DIR}/ora.sh
+_SPINNER_POS=0
+_TASK_OUTPUT=""
+CYAN=$(tput setaf 6)
+NORMAL=$(tput sgr0)
+BOLD=$(tput bold)
+
+ERROR_COLOR=`tput setaf 1`
+RUNNING_COLOR=`tput setaf 6`
+SUCCESS_COLOR=`tput setaf 2`
+NO_COLOR=`tput sgr0` # No Color
+
+spinner() {
+    _TASK_OUTPUT=""
+    local delay=0.05
+    local list=( $(echo -e '\xe2\xa0\x8b')
+                 $(echo -e '\xe2\xa0\x99')
+                 $(echo -e '\xe2\xa0\xb9')
+                 $(echo -e '\xe2\xa0\xb8')
+                 $(echo -e '\xe2\xa0\xbc')
+                 $(echo -e '\xe2\xa0\xb4')
+                 $(echo -e '\xe2\xa0\xa6')
+                 $(echo -e '\xe2\xa0\xa7')
+                 $(echo -e '\xe2\xa0\x87')
+                 $(echo -e '\xe2\xa0\x8f'))
+    local i=$_SPINNER_POS
+    local tempfile
+    tempfile=$(mktemp)
+
+    eval $2 >> $tempfile 2>/dev/null &
+    local pid=$!
+
+    tput sc
+    printf "%s %s" "${list[i]}" "$1"
+	tput el
+    tput rc
+
+    i=$(($i+1))
+    i=$(($i%10))
+
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        printf "%s" "${CYAN}${list[i]}${NORMAL}"
+        i=$(($i+1))
+        i=$(($i%10))
+        sleep $delay
+        printf "\b\b\b"
+    done
+    _TASK_OUTPUT="$(cat $tempfile)"
+    rm $tempfile
+    _SPINNER_POS=$i
+
+    if [ -z $3 ]; then :; else
+      eval $3=\'"$_TASK_OUTPUT"\'
+    fi
+}
 
 TEMP_FILE="/tmp/opsworks_deploy.tmp"
 
@@ -32,6 +85,12 @@ do
 	REGION=""
 	PROFILE=""
 
+	if [ ! -f ${DIR}/$var ]
+	then
+		echo "${ERROR_COLOR}Variables File Not Found (${DIR}/${var})${NO_COLOR}"
+		exit
+	fi
+
 	source ${DIR}/$var;
 
 	if [ -z "$TITLE" ]
@@ -49,14 +108,6 @@ done
 
 APPS_STRING="$(printf ", %s" "${DEPLOYMENT_TITLES[@]}")"
 APPS_STRING="${APPS_STRING:2}"
-
-
-
-
-ERROR_COLOR=`tput setaf 1`
-RUNNING_COLOR=`tput setaf 6`
-SUCCESS_COLOR=`tput setaf 2`
-NO_COLOR=`tput sgr0` # No Color
 
 RETRY_LIMIT=60;
 WAIT_TIME=2s;
